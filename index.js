de18b16d8fb2ff14369d257b2717b06075b75f0c
leonardotc <leonardo.t.cardoso@hotmail.com>
2019-08-04 14:33:14 -0300

    First project steps

---
+++ index.js
@@ -0,0 +1,9 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div className='blue'>{'Hello'}</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
