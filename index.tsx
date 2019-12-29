eb3d56520f6ffe940af79581f67a1456c045c8a9
leonardotc <leonardo.t.cardoso@hotmail.com>
2019-12-29 14:28:51 -0200

    Change sortable version

--- index.tsx
+++ index.tsx
@@ -1,43 +1,59 @@
 import './index.css'
-import React from 'react'
+import React, { FC, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { filter } from 'ramda'
+import { ReactSortable, Sortable } from "react-sortablejs";
+import { uniqueId } from 'lodash'
 
-// _
-const columnData = [
+interface Card {
+  id: number;
+  name: string;
+  column_id: number;
+}
+
+interface Column {
+  id: number;
+  name: string;
+}
+
+const columns = [
   {id: 1, name: 'TO DO'},
   {id: 2, name: 'DOING'},
   {id: 3, name: 'DONE'}
 ]
 
-const cardData = [
+const cards = [
   {id: 1, name: 'Cook', column_id: 1 },
   {id: 2, name: 'Shop', column_id: 1},
   {id: 3, name: 'Fly', column_id: 2 }
 ]
 
-const Card = ({data}) => {
-  return <div className="card">{data.name}</div>
+const Card = ({ data }) => {
+  return (
+    <div className="card" key={data.id}>{data.name}</div>
+  )
 }
 
 const Column = ({ data }) => {
   const filterByCurrentColumnId = filter(card => card.column_id === data.id)
-  let currentColumnCards = filterByCurrentColumnId(cardData)
+  const [state, setState] = useState<Card[]>(filterByCurrentColumnId(cards))
+
   return (
     <div className='column'>
       <div className="column-header">{data.name}</div>
-      <div className="column-body">
-        {currentColumnCards.map(card=> <Card data={card} />)}
-      </div>
+      <ReactSortable list={state} setList={setState} group="cards" className="column-body">
+        {state.map(card=> <Card data={card} key={card.id} />)}
+      </ReactSortable>
     </div>
   )
 }
 
 const App = () => {
+  const [state, setState] = useState<Column[]>(columns)
   return (
-    <div class="board">
-      {columnData.map( columnData => <Column data={columnData} />)}
-    </div>
+      <ReactSortable list={state} setList={setState} group="columns" className="board">
+        {state.map(column => <Column data={column} key={column.id} />)}
+      </ReactSortable>
   )
 }
 
